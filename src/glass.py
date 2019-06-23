f506fe81a3bc107209701e01374538ebd329c3f2
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2019-06-23 19:16:35 +0800

    Structuring the Glass node

--- src/glass.py
+++ src/glass.py
@@ -12,15 +12,19 @@ class Glass:
 
     def __init__(self, capacity: float=0.25):
 
-        self.left: Glass = None
-        self.right: Glass = None
-        self.parent: Glass = None
+        self.left_child: Glass = None
+        self.right_child: Glass = None
+
+        self.left_parent: Glass = None
+        self.right_parent: Glass = None
 
         self.capacity: float = capacity
         self.water: float = 0
 
     def fill(self, k: float):
+        """ Fill this glass with k litres of water. """
 
+        # Work out how much can go into this glass, and how much shall overflow.
         remaining_capacity = self.capacity - self.water
         overflow = max(0.0, k - remaining_capacity)
         fill_amount = k - overflow
@@ -34,10 +38,47 @@ class Glass:
         self._fill_child(left=False, k=half_overflow)
 
     def _fill_child(self, left: bool=True, k: float=0):
-        pass
+        """ Fill the child glass with k litres of water. """
 
-    def _create_child(self):
-        pass
+        if k <= 0:
+            return
 
-    def _link_child(self):
-        pass
+        # We need to create the child if it doesn't exist.
+        self._ensure_child(left)
+        child = self.left_child if left else self.right_child
+        child.fill(k)
+
+    def _ensure_child(self, left: bool=True):
+        """ Ensure that a child exists. If it does not, create it."""
+
+        # If the child already exists, then we are done.
+        if left and self.left_child is not None:
+            return
+        elif not left and self.right_child is not None:
+            return
+
+        # Create the child and refer to it.
+        child = Glass(self.capacity)
+        if left:
+            self.left_child = child
+        else:
+            self.right_child = child
+
+        # It must also be bound to its siblings.
+        self._link_child(left)
+
+    def _link_child(self, left: bool) -> None:
+        """ After a child has been created, it should also be the child of the sibling node. """
+
+        parent = self.left_parent if left else self.right_parent
+        if parent is None:
+            return
+
+        sibling = parent.left_child if left else parent.right_child
+        if sibling is None:
+            return
+
+        if left:
+            sibling.right_child = self.left_child
+        else:
+            sibling.left_child = self.right_child
